64dc0c146ba5cc18e14aafd09903f3229c7f2374
damian1993199 <dfernandez16@abc.gob.ar>
2024-05-23 21:13:02 -0300

    Add files via upload
    1

---
+++ src/main/java/com/cine/message/MessageApplication.java
@@ -0,0 +1,30 @@
+package com.cine.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+
+}
